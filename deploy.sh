66a507ddc0768c9d7fe32259d1b4c4ed25117f64
lonclegr <lonclegr@linux.home>
2017-08-12 22:54:39 +0200

    start IHM and deply scrit

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+[ -z "$1" ] && echo "Error : need one argument" && exit 1
+
+case "$1" in
+  "local")
+    echo "Deploying code locally"
+    sudo cp -v *.php /var/www/html/ged/
+    sudo cp -v /home/lonclegr/Images/ged/.ged.db /var/www/html/ged/
+  ;;
+  *)
+    echo "Unknow option! Try again with local for instance"
+    exit 1
+esac
--- index.php
+++ index.php
@@ -43,7 +43,7 @@ if (php_sapi_name() == "cli") {
 
   # SQLite3
   $db = new SQLite3(sprintf('%s%s%s', $folderPath, DIRECTORY_SEPARATOR, '.ged.db'));
-  $db->exec('CREATE TABLE if not exists myFiles (label STRING)');
+  $db->exec('CREATE TABLE if not exists myFiles (label STRING, slug STRING)');
 
   echo sprintf("Listing folder %s\n", $folderPath);
   foreach ($listFolder as $content) {
@@ -53,16 +53,16 @@ if (php_sapi_name() == "cli") {
         echo "\t";
         echo sprintf("%s\n", $content);
 				$mySlugifiedText = slugify($content);
-				$arrayResult = $db->querySingle(sprintf('select * from myFiles where label = "%s"', $mySlugifiedText));
+				$arrayResult = $db->querySingle(sprintf('select * from myFiles where slug = "%s"', $mySlugifiedText));
 				if (empty($arrayResult)) {
-					$db->exec(sprintf('insert into myFiles (label) values ("%s")', $mySlugifiedText));
+					$db->exec(sprintf('insert into myFiles (label, slug) values ("%s", "%s")', $content, $mySlugifiedText));
 				}
       }
     }
   }
 
   echo sprintf("Listing DB content\n");
-  $result = $db->query("select label from myFiles");
+  $result = $db->query("select * from myFiles");
   while ($myResult = $result->fetchArray()){
      print_r($myResult);
   }
@@ -70,4 +70,26 @@ if (php_sapi_name() == "cli") {
   $db->close();
 } else {
   # if php and httpd
+  error_reporting(E_ALL);
+  ini_set('display_errors', '1');
+  echo "<!doctype html>";
+  echo "<html>";
+  echo "<head>";
+  echo sprintf("<title>GED</title>");
+  echo "</head>";
+  echo "<body>";
+
+  # SQLite3
+  echo sprintf("<h2>%s</h2>", "Media listing");
+  $folderPath = "/var/www/html/ged/";
+  $db = new SQLite3(sprintf('%s%s%s', $folderPath, DIRECTORY_SEPARATOR, '.ged.db'));
+  $result = $db->query("select label from myFiles order by label");
+  echo "<ul>";
+  while ($myResult = $result->fetchArray()){
+    echo sprintf("<li>%s</li>", $myResult[0]);
+  }
+  echo "</ul>";
+
+  echo "</body>";
+  echo "</html>";
 }
